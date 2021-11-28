c2ea2085d285dace712b6f479f28551fba38ce84
Lauana Hubler <lauanahubler@gmail.com>
2021-11-28 16:19:51 -0500

    style: php-cs-fixer

--- src/Helpers/DriverHelper.php
+++ src/Helpers/DriverHelper.php
@@ -12,8 +12,8 @@ class DriverHelper
         $driverType ??= config('dracma.driver');
 
         $className = array_reduce(explode('_', $driverType), function ($name, $word) {
-                return $name.ucfirst($word);
-            }, 'LauanaOH\\Dracma\\Drivers\\').'Driver';
+            return $name.ucfirst($word);
+        }, 'LauanaOH\\Dracma\\Drivers\\').'Driver';
 
         return class_exists($className) ? new $className() : new NullDriver();
     }
